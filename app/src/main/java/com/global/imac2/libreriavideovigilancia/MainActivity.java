a8487ab1fd54bc061eb6901615491d25691867a7
Gusstavo1 <ipngustavo@gmail.com>
2019-08-21 09:14:29 -0500

    Initial commit

---
+++ app/src/main/java/com/global/imac2/libreriavideovigilancia/MainActivity.java
@@ -0,0 +1,61 @@
+package com.global.imac2.libreriavideovigilancia;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+//import com.global.prueba.LaunchVideovigilancia;
+//import com.global.imac2.integracionvideovigilancia.LaunchVideovigilancia;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtCorreo, edtPass;
+    private Button btnIntegracion, btnDescarga;
+    //private LaunchVideovigilancia launchVideovigilancia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*launchVideovigilancia = new LaunchVideovigilancia();
+
+        btnIntegracion = (Button)findViewById(R.id.btnIntegracion);
+        btnDescarga = (Button)findViewById(R.id.btnDescarga);
+        edtCorreo = (EditText)findViewById(R.id.edtCorreo);
+        edtPass = (EditText)findViewById(R.id.edtPass);
+        edtPass.setVisibility(View.GONE);
+        edtCorreo.setVisibility(View.GONE);
+
+        AlertDialog alertDialog = AlertaIntegracion();
+        alertDialog.show();
+
+        btnIntegracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean statusIntegracion = launchVideovigilancia.StartVigilancia(getApplicationContext());
+                Log.e("Integracion",  ""+statusIntegracion);
+            }
+        });
+
+        btnDescarga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchVideovigilancia.DescargaVideoVigilancia(getApplicationContext());
+            }
+        });*/
+    }
+
+    /*private AlertDialog AlertaIntegracion(){
+        AlertDialog.Builder builder =new AlertDialog.Builder(this);
+        builder.setTitle("¿App instalada? ");
+        builder.setMessage("Status: " + launchVideovigilancia.ValidacionVideoVigilanciaInstalada(getApplicationContext()));
+        return builder.create();
+    }*/
+}
---
+++ integracionvideovigilancia/src/main/java/com/global/imac2/integracionvideovigilancia/AlertaIntegracion.java
@@ -0,0 +1,52 @@
+package com.global.imac2.integracionvideovigilancia;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class AlertaIntegracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alerta_integracion);
+
+        AlertDialog alertDialog = AlertaInstalaSOS();
+        alertDialog.show();
+
+    }
+
+    private AlertDialog AlertaInstalaSOS(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setTitle("Inicie videovigilancia.")
+                .setMessage("Puede instalar el modulo de Videovigilancia compatible con Claro360.")
+                .setNegativeButton("Instalar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        LanzaDescarga();
+                    }
+                })
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        finish();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    private void LanzaDescarga() {
+        String urlDescarga = getResources().getString(R.string.URL_VIDEOPLAY);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlDescarga));
+        startActivity(intent);
+    }
+
+}
