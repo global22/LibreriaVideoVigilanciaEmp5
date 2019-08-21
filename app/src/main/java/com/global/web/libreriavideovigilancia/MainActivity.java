b77e5ae5e651e1cb8eb2caea8d96332669647169
global22 <global.soportepc@gmail.com>
2019-08-21 11:12:46 -0500

    cambio de import

--- app/src/main/java/com/global/web/libreriavideovigilancia/MainActivity.java
+++ app/src/main/java/com/global/web/libreriavideovigilancia/MainActivity.java
@@ -9,20 +9,21 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.global.imac2.libreriavideovigilancia.R;
+import com.global.web.integracionvideovigilancia.LaunchVideovigilancia;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText edtCorreo, edtPass;
     private Button btnIntegracion, btnDescarga;
-    //private LaunchVideovigilancia launchVideovigilancia;
+    private LaunchVideovigilancia launchVideovigilancia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //launchVideovigilancia = new LaunchVideovigilancia();
+        launchVideovigilancia = new LaunchVideovigilancia();
 
         btnIntegracion = (Button)findViewById(R.id.btnIntegracion);
         //btnDescarga = (Button)findViewById(R.id.btnDescarga);
@@ -31,29 +32,29 @@ public class MainActivity extends AppCompatActivity {
         edtPass.setVisibility(View.GONE);
         edtCorreo.setVisibility(View.GONE);
 
-        //AlertDialog alertDialog = AlertaIntegracion();
-        //alertDialog.show();
+        AlertDialog alertDialog = AlertaIntegracion();
+        alertDialog.show();
 
-        /*btnIntegracion.setOnClickListener(new View.OnClickListener() {
+        btnIntegracion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Boolean statusIntegracion = launchVideovigilancia.StartVigilancia(getApplicationContext());
                 Log.e("Integracion",  ""+statusIntegracion);
             }
-        });*/
+        });
 
-        /*btnDescarga.setOnClickListener(new View.OnClickListener() {
+        btnDescarga.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 launchVideovigilancia.DescargaVideoVigilancia(getApplicationContext());
             }
-        });*/
+        });
     }
 
-    /*private AlertDialog AlertaIntegracion(){
+    private AlertDialog AlertaIntegracion(){
         AlertDialog.Builder builder =new AlertDialog.Builder(this);
         builder.setTitle("¿App instalada? ");
         builder.setMessage("Status: " + launchVideovigilancia.ValidacionVideoVigilanciaInstalada(getApplicationContext()));
         return builder.create();
-    }*/
+    }
 }
