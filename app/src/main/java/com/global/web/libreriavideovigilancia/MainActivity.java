ff2f2c8e017190e0356fa91cec4c1b98db182c0d
global22 <global.soportepc@gmail.com>
2019-08-30 17:09:42 -0500

    se quito webview

--- app/src/main/java/com/global/web/libreriavideovigilancia/MainActivity.java
+++ app/src/main/java/com/global/web/libreriavideovigilancia/MainActivity.java
@@ -24,9 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        webView = (WebView)findViewById(R.id.webview);
-
+        
         launchVideovigilancia = new LaunchVideovigilancia();
 
         btnIntegracion = (Button)findViewById(R.id.btnIntegracion);
